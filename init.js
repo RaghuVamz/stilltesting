ef99bf324008ad8190e33c95466b08f5f68df787
johnbonda <john@belfricsbt.com>
2018-10-29 11:20:46 +0530

    updated inti.js

--- init.js
+++ init.js
@@ -1,9 +1,9 @@
 module.exports = async function () {
   console.log('enter dapp init')
 
-  app.registerContract(1001, 'hello.request')
-  app.registerContract(1002, 'hello.respond')
   app.registerContract(1003, 'payroll.issuePaySlip')
+  app.registerContract(1004, 'payroll.verify')
+
 
   app.events.on('newBlock', (block) => {
     console.log('new block receive', block.height)
