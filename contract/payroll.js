2a01f427dbeeb9160a0921ccfc680d096709ee13
johnbonda <john@belfricsbt.com>
2018-10-30 16:45:25 +0530

    Changed Port

--- contract/payroll.js
+++ contract/payroll.js
@@ -61,7 +61,7 @@ module.exports = {
     verify: async function(objtext){
         
         //app.logger.debug(objtext);
-        console.log(obtext);
+        console.log(objtext);
         var obj = JSON.parse(objtext);
         var hash = util.getHash(obj);
         //var hash = util.getHash(objtext);
